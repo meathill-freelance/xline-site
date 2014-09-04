67f9008dc5b98a9d8dba5fe6fccb6bc29f26f99c
Meathill <meathill@gmail.com>
2014-09-05 00:34:51 +0800

    修复若干小问题
    Signed-off-by: Meathill <meathill@gmail.com>

--- footer.php
+++ footer.php
@@ -10,6 +10,14 @@
 
 require_once(dirname(__FILE__) . '/inc/Spokesman.class.php');
 
+if (!is_user_logged_in()) {
+  function add_backbone_js() {
+    echo '<script src="//cdn.staticfile.org/underscore.js/1.6.0/underscore-min.js"></script>';
+    echo '<script src="//cdn.staticfile.org/backbone.js/1.1.2/backbone-min.js"></script>';
+  }
+  add_action('xline_footer', 'add_backbone_js');
+}
+
 $result = array(
   'theme_url' => get_template_directory_uri(),
 );
